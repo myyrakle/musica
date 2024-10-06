0867d9d11cfe12abc8aedcab2b1477fc6043359c
myyrakle <sssang97@naver.com>
2024-10-06 11:13:25 +0900

    [#39] app id 지정

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,15 @@ fn main() -> iced::Result {
     let config_path = config::get_config_path();
     config::create_config_if_not_exists(config_path).unwrap();
 
-    let setting = Settings::default();
+    let mut setting = Settings::default();
+    setting.id = Some("musica".into());
+
+    let mut window_setting = iced::window::Settings::default();
+    window_setting.platform_specific.application_id = "musica".into();
 
     iced::application("musica", MainApp::update, MainApp::view)
         .settings(setting)
+        .window(window_setting)
         .resizable(false)
         .window_size(Size::new(300.0, 600.0))
         .theme(MainApp::theme)
