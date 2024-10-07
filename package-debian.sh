c76473371aa8b6258d9919e56f52bdd7d15595b1
myyrakle <sssang97@naver.com>
2024-10-08 00:49:48 +0900

    [#38] shell 코드 개선

--- package-debian.sh
+++ package-debian.sh
@@ -1,3 +1,4 @@
 cargo build --release 
+mkdir -p dist/debian-package/usr/local/bin
 mv target/release/musica-app dist/debian-package/usr/local/bin/musica
 dpkg-deb --build dist/debian-package dist/musica.deb
